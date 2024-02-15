b6bbb84c98fcea3c6f8907ba3c2b86b23f116145
Na3ar-17 <gavruk2017@gmail.com>
2024-02-15 21:08:45 +0200

    configured nodemaile

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
+import nodemailer from 'nodemailer'
+
 const app = express()
+dotenv.config()
 
 app.use(express.json())
 app.use(cors())
 
 const PORT = process.env.PORT || 4000
 
+const transporter = nodemailer.createTransport({
+  service: 'google',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+})
+
+const options = {
+  from: 'gavruluknazar0210@gmail.com',
+  to: 'gavruluknazar0210@gmail.com',
+  subject: 'test',
+  text: 'test',
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  transporter.sendMail(options, (err) => {
+    console.log(err)
+  })
 })
 
 try {
